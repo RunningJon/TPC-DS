e4a67cff114d8bcc2a1d68b839ae15ccf8167ed4
jgronline@gmail.com <jgronline@gmail.com>
2015-10-14 14:59:21 -0500

    0.0.11

--- 06_reports/rollout.sh
+++ 06_reports/rollout.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -e
-
 PWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+source $PWD/../functions.sh
 
 for i in $(ls $PWD/*.sql | grep -v report.sql); do
 	table_name=`echo $i | awk -F '.' '{print $3}'`
 	CMD="'cat $PWD/../log/rollout_$table_name.log'"
-	echo "psql -a -f $i -v CMD=\"$CMD\""
-	psql -a -f $i -v CMD="$CMD"
+	echo "psql -h $MASTER_HOST -a -f $i -v CMD=\"$CMD\""
+	psql -h $MASTER_HOST -a -f $i -v CMD="$CMD"
 	echo ""
 done
 
-psql -P pager=off -f $PWD/detailed_report.sql
+psql -h $MASTER_HOST -P pager=off -f $PWD/detailed_report.sql
 echo ""
-psql -P pager=off -f $PWD/summary_report.sql
+psql -h $MASTER_HOST -P pager=off -f $PWD/summary_report.sql
