f9716dd89293e210040a14142ac72817728084ae
jgronline@gmail.com <jgronline@gmail.com>
2015-10-16 10:44:13 -0500

    0.0.15

--- 02_init/rollout.sh
+++ 02_init/rollout.sh
@@ -13,7 +13,7 @@ table_name="init"
 update_config="0"
 
 #check optimizer
-counter=`gpconfig -s optimizer | grep Master | grep on | wc -l`
+counter=$(gpconfig -s optimizer | grep Master | grep on | wc -l)
 if [ "$counter" -eq "0" ]; then
 	echo "enabling optimizer"
 	gpconfig -c optimizer -v on --masteronly
@@ -21,7 +21,7 @@ if [ "$counter" -eq "0" ]; then
 fi
 
 #check analyze_root_partition
-counter=`gpconfig -s optimizer_analyze_root_partition | grep Master | grep on | wc -l`
+counter=$(gpconfig -s optimizer_analyze_root_partition | grep Master | grep on | wc -l)
 if [ "$counter" -eq "0" ]; then
 	echo "enabling analyze_root_partition"
 	gpconfig -c optimizer_analyze_root_partition -v on --masteronly
