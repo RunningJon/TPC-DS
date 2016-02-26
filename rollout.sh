1ee53a5980d89dc33787d0280398fa56ca50871e
jgronline@gmail.com <jgronline@gmail.com>
2016-02-26 14:01:23 -0600

    2.1.14

--- rollout.sh
+++ rollout.sh
@@ -14,7 +14,7 @@ RUN_GEN_DATA="$6"
 RUN_INIT="$7"
 RUN_DDL="$8"
 RUN_LOAD="$9"
-RUN_SQL="$10"
+RUN_SQL=${10}
 
 if [[ "$GEN_DATA_SCALE" == "" || "$EXPLAIN_ANALYZE" == "" || "$SQL_VERSION" == "" || "$RANDOM_DISTRIBUTION" == "" || "$RUN_COMPILE_TPCDS" == "" || "$RUN_GEN_DATA" == "" || "$RUN_INIT" == "" || "$RUN_DDL" == "" || "$RUN_LOAD" == "" || "$RUN_SQL" == "" ]]; then
 	echo "You must provide the scale as a parameter in terms of Gigabytes, true/false to run queries with EXPLAIN ANALYZE option, the SQL_VERSION, and true/false to use random distrbution."
