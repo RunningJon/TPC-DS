d5633331d15e96d9ccbf880c2426a08b5a0520fa
Prince Kannah <PrinceKannah@u.boisestate.edu>
2016-04-28 16:28:17 -0600

    Update stop_gpfdist.sh

--- 04_load/stop_gpfdist.sh
+++ 04_load/stop_gpfdist.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+source /usr/local/greenplum-db-4.3.7.1/greenplum_path.sh
 
 for i in $(ps -ef | grep gpfdist |  grep -v grep | grep -v stop_gpfdist | awk -F ' ' '{print $2}'); do
         echo "killing $i"
