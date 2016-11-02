e8b7908e4ceedc2a6d7e423399a2a8dc6bd4fe3c
Jemish Patel <jpatel@pivotal.io>
2016-11-02 12:57:03 -0700

    Update rollout.sh
    fix for host key change warning

--- 00_compile_tpcds/rollout.sh
+++ 00_compile_tpcds/rollout.sh
@@ -43,7 +43,7 @@ copy_tpc()
 	#copy the compiled dsdgen program to the segment hosts
 	for i in $(cat $PWD/../segment_hosts.txt); do
 		echo "copy tpcds binaries to $i:$ADMIN_HOME"
-		scp tools/dsdgen tools/tpcds.idx $i:$ADMIN_HOME/
+		scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no tools/dsdgen tools/tpcds.idx $i:$ADMIN_HOME/
 	done
 }
 
