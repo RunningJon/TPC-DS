4f4575c5b1d4330fff495668b8f292b608ea3386
Jon Roberts <jgronline@gmail.com>
2018-07-11 16:34:35 -0400

    2.4.0

--- 08_multi_user_reports/rollout.sh
+++ 08_multi_user_reports/rollout.sh
@@ -8,13 +8,32 @@ step="multi_user_reports"
 
 init_log $step
 
-for i in $(ls $PWD/*.sql | grep -v report.sql); do
-        schema_name=$(echo $i | awk -F '.' '{print $2}')
-	EXECUTE="'cat $PWD/../log/rollout_$schema_name*.log'"
-        echo "psql -a -f $i -v EXECUTE=\"$EXECUTE\""
-        psql -a -f $i -v EXECUTE="$EXECUTE"
-        echo ""
+get_version
+if [[ "$VERSION" == *"gpdb"* ]]; then
+	filter="gpdb"
+elif [ "$VERSION" == "postgresql" ]; then
+	filter="postgresql"
+else
+	echo "ERROR: Unsupported VERSION!"
+	exit 1
+fi
+
+for i in $(ls $PWD/*.$filter.*.sql); do
+	echo "psql -a -f $i"
+	psql -a -f $i
+	echo ""
 done
+
+filename=$(ls $PWD/*.copy.*.sql)
+
+for i in $(ls $PWD/../log/rollout_testing_*); do
+	logfile="'""$i""'"
+	
+        echo "psql -a -f $filename -v LOGFILE=\"$logfile\""
+        psql -a -f $filename -v LOGFILE="$logfile"
+done
+
+psql -t -A -c "select 'analyze ' || n.nspname || '.' || c.relname || ';' from pg_class c join pg_namespace n on n.oid = c.relnamespace and n.nspname = 'tpcds_testing'" | psql -t -A -e
 
 psql -F $'\t' -A -P pager=off -f $PWD/detailed_report.sql
 echo ""
