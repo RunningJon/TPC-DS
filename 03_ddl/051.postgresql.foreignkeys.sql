224b9bf9ee1f4ec81e3e84597c604d306b0b9f07
Jon Roberts <jgronline@gmail.com>
2018-07-12 14:07:59 -0400

    2.4.0

--- 03_ddl/051.postgresql.foreignkeys.sql
+++ 03_ddl/051.postgresql.foreignkeys.sql
@@ -1,4 +1,4 @@
-#store_sales
+--store_sales
 alter table store_sales add foreign key (ss_sold_date_sk) references date_dim (d_date_sk);
 alter table store_sales add foreign key (ss_sold_time_sk) references time_dim (t_time_sk);
 alter table store_sales add foreign key (ss_item_sk) references item (i_item_sk);
@@ -9,7 +9,7 @@ alter table store_sales add foreign key (ss_addr_sk) references customer_address
 alter table store_sales add foreign key (ss_store_sk) references store (s_store_sk);
 alter table store_sales add foreign key (ss_promo_sk) references promotion (p_promo_sk);
 
-#store_returns
+--store_returns
 alter table store_returns add foreign key (sr_returned_date_sk) references date_dim (d_date_sk);
 alter table store_returns add foreign key (sr_return_time_sk) references time_dim (t_time_sk);
 alter table store_returns add foreign key (sr_item_sk) references item (i_item_sk);
@@ -21,7 +21,7 @@ alter table store_returns add foreign key (sr_addr_sk) references customer_addre
 alter table store_returns add foreign key (sr_store_sk) references store (s_store_sk);
 alter table store_returns add foreign key (sr_reason_sk) references reason (r_reason_sk);
 
-#catalog_sales
+--catalog_sales
 alter table catalog_sales add foreign key (cs_sold_date_sk) references date_dim (d_date_sk);
 alter table catalog_sales add foreign key (cs_sold_time_sk) references time_dim (t_time_sk);
 alter table catalog_sales add foreign key (cs_ship_date_sk) references date_dim (d_date_sk);
@@ -40,7 +40,7 @@ alter table catalog_sales add foreign key (cs_warehouse_sk) references warehouse
 alter table catalog_sales add foreign key (cs_item_sk) references item (i_item_sk);
 alter table catalog_sales add foreign key (cs_promo_sk) references promotion (p_promo_sk);
 
-#catalog_returns
+--catalog_returns
 alter table catalog_returns add foreign key (cr_returned_date_sk) references date_dim (d_date_sk);
 alter table catalog_returns add foreign key (cr_return_time_sk) references time_dim (t_time_sk);
 alter table catalog_returns add foreign key (cr_item_sk) references item (i_item_sk);
@@ -58,7 +58,7 @@ alter table catalog_returns add foreign key (cr_ship_mode_sk) references ship_mo
 alter table catalog_returns add foreign key (cr_warehouse_sk) references warehouse (w_warehouse_sk);
 alter table catalog_returns add foreign key (cr_reason_sk) references reason (r_reason_sk);
 
-#web_sales
+--web_sales
 alter table web_sales add foreign key (ws_sold_date_sk) references date_dim (d_date_sk);
 alter table web_sales add foreign key (ws_sold_time_sk) references time_dim (t_time_sk);
 alter table web_sales add foreign key (ws_ship_date_sk) references date_dim (d_date_sk);
@@ -77,7 +77,7 @@ alter table web_sales add foreign key (ws_ship_mode_sk) references ship_mode (sm
 alter table web_sales add foreign key (ws_warehouse_sk) references warehouse (w_warehouse_sk);
 alter table web_sales add foreign key (ws_promo_sk) references promotion (p_promo_sk);
 
-#web_returns
+--web_returns
 alter table web_returns add foreign key (wr_returned_date_sk) references date_dim (d_date_sk);
 alter table web_returns add foreign key (wr_returned_time_sk) references time_dim (t_time_sk);
 alter table web_returns add foreign key (wr_item_sk) references item (i_item_sk);
@@ -93,43 +93,43 @@ alter table web_returns add foreign key (wr_returning_addr_sk) references custom
 alter table web_returns add foreign key (wr_web_page_sk) references web_page (wp_web_page_sk);
 alter table web_returns add foreign key (wr_reason_sk) references reason (r_reason_sk);
 
-#inventory
+--inventory
 alter table inventory add foreign key (inv_date_sk) references date_dim (d_date_sk);
 alter table inventory add foreign key (inv_item_sk) references time_dim (t_time_sk);
 alter table inventory add foreign key (inv_warehouse_sk) references warehouse (w_warehouse_sk);
 
-#store
+--store
 alter table store add foreign key (s_closed_date_sk) references date_dim (d_date_sk);
 
-#call_center
+--call_center
 alter table call_center add foreign key (cc_closed_date_sk) references date_dim (d_date_sk);
 alter table call_center add foreign key (cc_open_date_sk) references date_dim (d_date_sk);
 
-#catalog_page
+--catalog_page
 alter table catalog_page add foreign key (cp_start_date_sk) references date_dim (d_date_sk);
 alter table catalog_page add foreign key (cp_end_date_sk) references date_dim (d_date_sk);
 
-#web_site
+--web_site
 alter table web_site add foreign key (web_open_date_sk) references date_dim (d_date_sk);
 alter table web_site add foreign key (web_close_date_sk) references date_dim (d_date_sk);
 
-#web_page
+--web_page
 alter table web_page add foreign key (wp_creation_date_sk) references date_dim (d_date_sk);
 alter table web_page add foreign key (wp_access_date_sk) references date_dim (d_date_sk);
 alter table web_page add foreign key (wp_customer_sk) references customer (c_customer_sk);
 
-#customer
+--customer
 alter table customer add foreign key (c_current_cdemo_sk) references customer_demographics (cd_demo_sk);
 alter table customer add foreign key (c_current_hdemo_sk) references household_demographics (hd_demo_sk);
 alter table customer add foreign key (c_current_addr_sk) references customer_address (ca_address_sk);
 alter table customer add foreign key (c_first_shipto_date_sk) references date_dim (d_date_sk);
 alter table customer add foreign key (c_first_sales_date_sk) references date_dim (d_date_sk);
-alter table customer add foreign key (c_last_review_date_sk) references date_dim (d_date_sk);
+alter table customer add foreign key (c_last_review_date) references date_dim (d_date_sk);
 
-#household_demographics
+--household_demographics
 alter table household_demographics add foreign key (hd_income_band_sk) references income_band (ib_income_band_sk);
 
-#promotion
+--promotion
 alter table promotion add foreign key (p_start_date_sk) references date_dim (d_date_sk);
 alter table promotion add foreign key (p_end_date_sk) references date_dim (d_date_sk);
 alter table promotion add foreign key (p_item_sk) references item (i_item_sk);
